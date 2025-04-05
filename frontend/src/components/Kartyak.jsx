a192da59c20eec337bed9a27fc9c6bc98ab80d26
BelaBordacs <side1111@hotmail.com>
2025-04-05 10:34:04 +0200

    vizsga end

--- frontend/src/components/Kartyak.jsx
+++ frontend/src/components/Kartyak.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useFeladatContext } from '../context/FeladatContext'
 import './kartyak.css'
 
 const Kartyak = () => {
     const {tesztek} = useFeladatContext([]);
     const {selectedId} = useFeladatContext(null);
+    //const [valaszok, setValaszok] = useState({});
+
+    /*var classNev = {
+        valasz: true,
+        helyes: false,
+        helytelen: false
+    }*/
 
     const filteredTesztek = selectedId ?
     tesztek.filter(e => e.kategoriaId.toString() === selectedId) : [];
 
     const handleClick = (e, valaszKulcs) => {
         if (e.helyes === valaszKulcs) {
-            console.log("helyes");
+                console.log("helyes");
         } else {
             console.log("helytelen");
         }
@@ -22,7 +29,7 @@ const Kartyak = () => {
         return (
             <div className='kartya' key={e.id}>
                 <p className='kerdes'>{e.kerdes}</p>
-                <p className='valasz' onClick={()=> handleClick(e,'v1')}>{e.v1}</p>
+                <p className='valasz' value={e} onClick={()=> handleClick(e,'v1')}>{e.v1}</p>
                 <p className='valasz' onClick={()=> handleClick(e,'v2')}>{e.v2}</p>
                 <p className='valasz' onClick={()=> handleClick(e,'v3')}>{e.v3}</p>
                 <p className='valasz' onClick={()=> handleClick(e,'v4')}>{e.v4}</p>
